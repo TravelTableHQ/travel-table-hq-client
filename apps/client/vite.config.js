8626e4d6b6ea2015aad0cc40fcf8a673f73bc162
r14minji <rkdalswl0728@gmail.com>
2025-06-22 15:51:34 +0900

    feat: Set up shadcn/ui in monorepo

--- apps/client/vite.config.js
+++ apps/client/vite.config.js
@@ -1,14 +1,23 @@
+import path from 'path'
+import { fileURLToPath } from 'url'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import Pages from 'vite-plugin-pages'
+import tailwindcss from 'tailwindcss'
 
-// https://vite.dev/config/
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     Pages({
       dirs: 'src/pages', // 페이지들이 위치할 폴더
       extensions: ['jsx', 'js'], // 지원할 파일 확장자
+    }),
+    tailwindcss({
+      config: path.resolve(__dirname, '../../packages/ui/tailwind.config.js'),
     }),
   ],
 })
