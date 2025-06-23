d1a7e1baf45da7b41839f3c43a3794b339a5e88f
r14minji <rkdalswl0728@gmail.com>
2025-06-23 21:53:31 +0900

    chore: 공통 개발 도구 패키지(ESLint, Prettier 등) 루트로 이동 및 하위 패키지 중복 제거

--- apps/client/vite.config.js
+++ apps/client/vite.config.js
@@ -1,10 +1,10 @@
+import tailwindcssPostcss from '@tailwindcss/postcss'
+import react from '@vitejs/plugin-react'
+import autoprefixer from 'autoprefixer'
 import path from 'path'
 import { fileURLToPath } from 'url'
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import Pages from 'vite-plugin-pages'
-import tailwindcssPostcss from '@tailwindcss/postcss'
-import autoprefixer from 'autoprefixer'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
@@ -15,21 +15,25 @@ export default defineConfig({
     react(),
     Pages({
       dirs: 'src/pages', // 페이지들이 위치할 폴더
-      extensions: ['jsx', 'js'], // 지원할 파일 확장자
+      extensions: ['ts', 'tsx'], // 지원할 파일 확장자 (ts, tsx로 변경)
+      importMode: 'async', // 코드 스플리팅을 위해 비동기 import
     }),
   ],
   css: {
     postcss: {
       plugins: [
         tailwindcssPostcss({
-          config: path.resolve(__dirname, '../../packages/ui/tailwind.config.js'),
+          config: path.resolve(
+            __dirname,
+            '../../packages/ui/tailwind.config.js'
+          ),
         }),
         autoprefixer,
       ],
     },
   },
   alias: {
-    '@': path.resolve(__dirname, './src'),           // 클라이언트 src
+    '@': path.resolve(__dirname, './src'), // 클라이언트 src
     '@tt/ui': path.resolve(__dirname, '../../packages/ui/src'), // UI 패키지
   },
 })
