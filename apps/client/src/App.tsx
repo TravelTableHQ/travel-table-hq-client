d7f0766713d093f3caa0fb76ba6e32347c1b41e9
r14minji <rkdalswl0728@gmail.com>
2025-06-23 22:58:37 +0900

    fix: AppRoutes 오류 수정

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -3,7 +3,11 @@ import { BrowserRouter } from 'react-router-dom'
 import AppRoutes from './router'
 
 function App() {
-  return <BrowserRouter>{AppRoutes()}</BrowserRouter>
+  return (
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
+  )
 }
 
 export default App
